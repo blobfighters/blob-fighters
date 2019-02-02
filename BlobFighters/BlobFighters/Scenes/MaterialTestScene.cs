4f19613934002b0555a222ae31462b0c50e5b39c
zosterday <zosterday@gmail.com>
2019-02-02 09:17:48 -0800

    input folder added

---
+++ BlobFighters/BlobFighters/Scenes/MaterialTestScene.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BlobFighters.Core;
+using BlobFighters.Objects;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BlobFighters.Scenes
+{
+    public class MaterialTestScene : Scene
+    {
+        public MaterialTestScene()
+        {
+        }
+
+        protected override void OnDestroy()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnDraw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnInit()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnUpdate(float deltaTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
